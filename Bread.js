90aa26907e904f7cf46c19ec575e20845227d30d
barlowtj48 <barlowtj48@gmail.com>
2021-03-20 04:29:32 -0400

    no message

--- Bread.js
+++ Bread.js
@@ -1,10 +1,16 @@
+import { inherits } from "node:util";
 import { get_loaf } from "./utils.js";
 
 
 export class Bread {
     constructor() { // get number of types of bread and pick from the selection
-        this.loaf = get_loaf();
-
+        let loaf = get_loaf();
+        this.intro_description = loaf.description;
+        this.loaf_name = loaf.name;
+        this.img_files = loaf.images;
+        this.state_descriptions = loaf.state_description;
+        this.value = loaf.value;
     }
+
 }
 
--- utils.js
+++ utils.js
@@ -1,13 +1,17 @@
 import glob from "glob";
 import path from "path";
+import { readFileSync, writeFileSync } from "fs";
+
 
 export function get_loaf() {
-    let dir = process.cwd();
+    let possible_entries = [];
     glob.sync(process.cwd() + "/bread_pics/*/*.json").forEach(async dir_file => {
-        console.log(dir_file);
-        let possible_entries = {}
-        data = JSON.parse(dir_file);
+        let data = JSON.parse(readFileSync(dir_file));
         possible_entries.push(data);
-        console.log(possible_entries);
     });
+    return possible_entries.random();
 }
+
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))];
+  }
