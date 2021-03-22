c656e711e0b77eea570fd125b850f02ba2185abe
Preston Smith <ptssmith2@gmail.com>
2021-03-22 03:36:20 -0400

    added in additional functions

--- Baker.js
+++ Baker.js
@@ -1,7 +1,19 @@
 import { Bread } from "Bread.js";
 
-export class Baker { 
-    constructor(member_id){
-        this.member_id = member_id;
+
+class Baker { 
+    constructor(id){
+        this.id = id;
+        this.balance = 0;
+        this.burned = 0;
+        this.successful = 0;
+        this.total_earned = 0;
+        this.my_bread = undefined;
+    }
+    make_bread(){
+        this.my_bread = new Bread();
+    }
+    is_baking(){
+        return this.my_bread != undefined;
     }
 }
