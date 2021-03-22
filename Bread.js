00a7ffcbba1726eec263744a946e38c38172822c
Preston Smith <ptssmith2@gmail.com>
2021-03-22 03:28:39 -0400

    Added in capability to get status of bread

--- Bread.js
+++ Bread.js
@@ -6,15 +6,43 @@ export class Bread {
     constructor() { // get number of types of bread and pick from the selection
         this.loaf = get_loaf(); //refers directly to the json files that hold all of the information
         this.state = 0;
+        this.start_time = new Date().getTime();
+
+        let len = this.loaf.images.length;
+        this.value = new Array(len).fill(0)
+        this.value[len-1] = -value;
+        this.value[len-2] = value;
+        this.value[len-3] = 1;
     }
 
     get_status(){
-        return this.loaf.state_description[this.state];
+        let now = new Date().getTime();
+        let difference = now - this.start_time;
+        if(this.state > this.loaf.images.length-1){
+            return this.loaf.state_description[this.loaf.images.length-1];
+        }
+        else{
+            this.state += Math.floor(difference/120000); //change state every 2 minutes until end of states
+            return this.loaf.state_description[this.state];
+        }
     }
 
     get_picture(){
         let dir = process.cwd();
-        return dir + "/bread_pics/" + this.loaf.folder_name + "/" + this.loaf.images[this.state];
+        return dir + "/bread_pics/" + this.loaf.folder_name + this.loaf.images[this.state];
     }
+
+    get_out_message(){
+        return this.loaf.state_description[this.state];
+    }
+
+    sell(){
+        return this.value[this.state];
+    }
+
+    
+
+
+
 }
 
